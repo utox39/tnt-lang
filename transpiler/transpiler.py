c3983f0210e00868d80f34db000db1b0c5010794
utox39 <francescomoccaldi39@gmail.com>
2026-06-04 17:21:33 +0200

    feat(transpiler): add the initial transpiler structure and an example of tnt code

---
+++ transpiler/transpiler.py
@@ -0,0 +1,20 @@
+from lark import Lark
+
+
+def main():
+    source_code = ""
+    with open("example.tnt") as f:
+        source_code = f.read()
+
+    grammar = ""
+    with open("tnt.lark") as f:
+        grammar = f.read()
+
+    parser = Lark(grammar, parser="earley")
+    tree = parser.parse(source_code)
+
+    print(tree.pretty())
+
+
+if __name__ == "__main__":
+    main()
